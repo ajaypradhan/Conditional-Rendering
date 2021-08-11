1a5f62c8bd3487a64540740e7fed6deb6c194f84
Ajay Pradhan <pradhanajay14@gmail.com>
2021-08-12 00:42:29 +0530

    challenge one done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,11 +5,6 @@ function App() {
   return (
     <div className="container">
       <h1>Hello</h1>
-      {/* <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form> */}
       <Login />
     </div>
   );
